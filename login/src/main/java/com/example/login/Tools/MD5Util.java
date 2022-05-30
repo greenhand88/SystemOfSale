b4761e256312b11a59b17f83ca8755e33551c2ce
zzj <598093760@qq.com>
2022-05-30 16:32:04 +0800

    父模块对依赖版本号的管理

---
+++ login/src/main/java/com/example/login/Tools/MD5Util.java
@@ -0,0 +1,13 @@
+package com.example.login.Tools;
+import org.apache.commons.codec.digest.DigestUtils;
+
+
+public class MD5Util {
+    public static String md5(String src){
+        return DigestUtils.md5Hex(src);
+    }
+    private static final String salt="zzjNb";
+    public static String process(String src){
+        return md5(src+salt);
+    }
+}
